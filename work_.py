4cabe5c19ab310f6b60abbf97ec2a020c2f53f88
oleksandra <oleksandrabodiak@gmail.com>
2026-04-19 19:03:03 +0300

    створення окремого файлу filtered_walk.json

--- work_.py
+++ work_.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 df = pd.read_csv('random_walk.csv')
 
 # print(df.head())
@@ -14,3 +15,6 @@ print(f"максимальна відстань: {max_dist}")
 print(f"середня відстань: {mean_dist}")
 print("\nточки з відстанню більшою за середню:")
 print(df[df['distance'] > mean_dist])
+df[df['distance'] > mean_dist].to_json('filtered_walk.json', orient='records', indent=4)
+
+
